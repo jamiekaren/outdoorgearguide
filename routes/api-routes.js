a846eab7cc6e96519237937f4a37b2ad83f35e8e
Jamie Ferrugiaro <missjamiekaren@gmail.com>
2019-09-29 21:51:59 -0400

    Didn't mean to work in master, made changes to API routes for scrapping

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -2,6 +2,19 @@ const db = require("../models");
 
 module.exports = function (app) {
 
+    //route to scrape our website for adata
+    app.get("/scrape", (req,res) =>{
+        
+        //we use axios to grab the body of the html
+        axios.get("https://www.outdoorgearlab.com/camping-and-hiking").then((response) =>{
+
+        //then we load that into cherio to create a shorthand selector
+        var $ = cheerio.load(response.data);
+
+        //then we gotta grab the right areas
+
+        });
+    });
 
 
 
