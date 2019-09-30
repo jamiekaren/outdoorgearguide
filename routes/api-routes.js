f8be367fb4db96e649ea11706b1b95ea24c4f09b
Jamie Ferrugiaro <missjamiekaren@gmail.com>
2019-09-29 22:20:35 -0400

    api routes now scrape website and save as an object result, also added app.js file

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,17 +1,51 @@
 const db = require("../models");
+//scraping tools
+//axios is promised-bsed library
+const axios = require("axios");
+const cheerio = require("cheerio");
 
 module.exports = function (app) {
 
     //route to scrape our website for adata
-    app.get("/scrape", (req,res) =>{
-        
+    app.get("/scrape", (req, res) => {
+
         //we use axios to grab the body of the html
-        axios.get("https://www.outdoorgearlab.com/camping-and-hiking").then((response) =>{
+        axios.get("https://www.outdoorgearlab.com/camping-and-hiking").then((response) => {
 
-        //then we load that into cherio to create a shorthand selector
-        var $ = cheerio.load(response.data);
+            // console.log(response);
 
-        //then we gotta grab the right areas
+            //then we load that into cherio to create a shorthand selector
+            var $ = cheerio.load(response.data);
+
+            //then we gotta grab the right areas of our website
+
+            $("article").each(function (i, element) {
+
+                // save to an empty result object
+                var result = {};
+
+                result.title = $(this)
+                    .children("a")
+                    .attr("href");
+
+                result.link = $(this)
+                    .children("a")
+                    .attr("href");
+
+                console.log(result);
+
+                //now let's make a new post in our database
+                db.Posts.create(result).then(function (dbPost) {
+                    console.log(dbPost);
+                })
+                    .catch((err) => {
+                        console.log(err);
+                    });
+                res.send("Scrape Complete");
+            });
+
+
+
 
         });
     });
