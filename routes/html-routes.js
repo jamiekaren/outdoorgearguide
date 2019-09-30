e5135b3bf81082a238c3dcf137622c915bcf0439
Jamie Ferrugiaro <missjamiekaren@gmail.com>
2019-09-29 20:26:41 -0400

    initial files

---
+++ routes/html-routes.js
@@ -0,0 +1,17 @@
+const path = require("path");
+
+module.exports = (app) => {
+
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.join(__dirname, "../../public/index.html"));
+    });
+
+
+    app.get("/saved", (req, res) => {
+        res.sendFile(path.join(__dirname, "../../public/saved.html"));
+    });
+
+
+
+};
